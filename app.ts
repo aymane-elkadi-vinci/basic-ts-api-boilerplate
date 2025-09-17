70b31d8745d57090a3f22f220880c48caba6361d
aymane-elkadi-vinci <aymane.elkadi@student.vinci.be>
2025-09-17 15:04:25 +0200

    Exercices 1.2

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
 import express from "express";
 
 import usersRouter from "./routes/users";
-import pizzaRouter from "./routes/pizzas";
 import filmRouter from "./routes/films";
 
 const app = express();
+
+/* Middleware pour compter le nombre de GET requests et les afficher 1.2*/
+let requestNumber = 0;
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    requestNumber++;
+    console.log(`GET counter : ${requestNumber}`);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
-app.use("/pizzas", pizzaRouter);
 app.use("/films", filmRouter);
+
+
 
 export default app;
 
